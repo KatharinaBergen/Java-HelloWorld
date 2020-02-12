1ec3c9948d12fbc232ca634308f0bf3f28c01094
KatharinaBergen <38838546+KatharinaBergen@users.noreply.github.com>
2020-02-12 09:04:59 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter name.");
+		String name = scan.next();
+		System.out.println("Hello " + name);
+		
+	}
+}
+
+
